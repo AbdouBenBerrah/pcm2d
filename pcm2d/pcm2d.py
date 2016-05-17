aefdb92b50b9b6c63a372b056a00b7ea9cd777af
Olivier Farges <olivier@olivier-farges.com>
2016-05-17 11:20:38 +0200

    ajout des fichiers pour le module pcm2d et les tests

---
+++ pcm2d/pcm2d.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+"""Module pcm2d"""
+
+class PcmStorage()
+    """A 2D PCM Storage has some properties:
+    - a lenght 'length'
+    - a width 'width'
+    - an absorption coefficient for both solid and liquid phase 'kas' and 'kal'
+    - a diffusion coefficient for both solid and liquid phase 'kds' and 'kdl'
+    - an anisotropy factor of the Heyney-Greenstein sacattering function for both solad and liquid phase 'gs' and 'gl'
+    - ..."""
