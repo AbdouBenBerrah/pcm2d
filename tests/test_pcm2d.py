874fa52edb0fcaa348f532aafd8a12cb4a42d62f
Olivier Farges <olivier@olivier-farges.com>
2016-05-17 17:04:28 +0200

    test de la fonction snell-descartes pour deux milieux de meme indice

--- tests/test_pcm2d.py
+++ tests/test_pcm2d.py
@@ -3,7 +3,7 @@
 from pcm2d.pcm2d import heyney_greenstein
 from pcm2d.pcm2d import snell_descartes
 import numpy as np
-
+from numpy.testing import assert_array_equal
 
 def test_heyney_greenstein():
     assert heyney_greenstein(0, 0.5) == 0.5
@@ -16,4 +16,4 @@ def test_snell_descartes():
     normal = np.array([[1],[0]])
     n1 = 1.
     n2 = 1.
-    assert snell_descartes(n1, n2, normal, theta) == np.array([[0.5],[0.5]])
+    assert_array_equal(snell_descartes(n1, n2, normal, theta), np.array([[np.pi/2],[np.pi/2]]))
