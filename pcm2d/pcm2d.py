5b0fcfd85af8952716185b0e18b90aa65b882024
Olivier Farges <olivier@olivier-farges.com>
2016-05-17 16:56:59 +0200

    ajout de la fonction de snell-descartes

--- pcm2d/pcm2d.py
+++ pcm2d/pcm2d.py
@@ -14,7 +14,26 @@ def heyney_greenstein(g, s):
     if g == 0:
         return s
     else:
-        return (1 + g * g - ((1 - g * g)/(1 + g * (2 * s - 1)))) / ( 2 * g)
+        return (1 + g**2 - ((1 - g**2)/(1 + g * (2 * s - 1)))) / ( 2 * g)
+
+
+
+def snell_descartes(n1, n2, normal, theta1):    
+    """This function compute the Snells-Descartes law for a refraction and
+    returns a refraction direction with respect to the refractive index of both material, 
+    the incident direction and the normal at the interface"""
+    theta2 = np.array([[0],[0]])
+    n1_n2 = n1 / n2
+    cosTheta1 = normal[0] * (- theta1[0]) + normal[1] * (- theta1[1])
+    cosTheta2 = sc.sqrt(1 - (n1_n2**2) * (1 - cosTheta1**2))
+    if cosTheta2 < 0:
+        theta2 = theta1 + (2 * cosTheta1) * normal
+    else:
+        theta2 = n1_n2 * theta1 + (n1_n2 * cosTheta1 + cosTheta2) * normal
+    return theta2
+
+
+    
 
 class PcmStorage:
     """A 2D PCM Storage has some properties:
--- tests/test_pcm2d.py
+++ tests/test_pcm2d.py
@@ -1,6 +1,8 @@
 #-*- coding: utf-8 -*-
 
 from pcm2d.pcm2d import heyney_greenstein
+from pcm2d.pcm2d import snell_descartes
+import numpy as np
 
 
 def test_heyney_greenstein():
@@ -8,3 +10,10 @@ def test_heyney_greenstein():
 
 def test_heyney_greenstein_2():
     assert heyney_greenstein(0.5, 0.5) == 0.5
+
+def test_snell_descartes():
+    theta = np.array([[np.pi/2],[np.pi/2]])
+    normal = np.array([[1],[0]])
+    n1 = 1.
+    n2 = 1.
+    assert snell_descartes(n1, n2, normal, theta) == np.array([[0.5],[0.5]])
