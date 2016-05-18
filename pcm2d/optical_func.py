6e5fdc01dd5fa29beaf5d26442d64bd807ca1bd2
Olivier Farges <olivier@olivier-farges.com>
2016-05-18 11:26:05 +0200

    feature propagation déconne

---
+++ pcm2d/optical_func.py
@@ -0,0 +1,31 @@
+#-*- coding: utf-8 -*-
+
+"""Module for scattering and refraction function"""
+
+import numpy as np
+import scipy as sc
+
+def heyney_greenstein(g, s):
+    """This function compute the Heyney-Greenstein scattering function and
+    returns a diffusion direction with respect to the anisotropy factor g 
+    and to a random number"""
+    if g == 0:
+        return s
+    else:
+        return (1 + g**2 - ((1 - g**2)/(1 + g * (2 * s - 1)))) / ( 2 * g)
+
+
+
+def snell_descartes(n1, n2, normal, theta1):    
+    """This function compute the Snells-Descartes law for a refraction and
+    returns a refraction direction with respect to the refractive index of both material, 
+    the incident direction and the normal at the interface"""
+    theta2 = np.array([[0],[0]])
+    n1_n2 = n1 / n2
+    cosTheta1 = normal[0] * (- theta1[0]) + normal[1] * (- theta1[1])
+    cosTheta2 = sc.sqrt(1 - (n1_n2**2) * (1 - cosTheta1**2))
+    if cosTheta2 < 0:
+        theta2 = theta1 + (2 * cosTheta1) * normal
+    else:
+        theta2 = n1_n2 * theta1 + (n1_n2 * cosTheta1 + cosTheta2) * normal
+    return theta2
--- pcm2d/pcm2d.py
+++ pcm2d/pcm2d.py
@@ -9,21 +9,22 @@ import scipy as sc
 import pcm2d.optical_func as opt
 
 class PcmStorage:
-    """A 2D PCM Storage has some properties:
+    """ A 2D PCM Storage has some properties:
     - a lenght 'length'
     - a width 'width'
     - an absorption coefficient for both solid and liquid phase 'kas' and 'kal'
     - a diffusion coefficient for both solid and liquid phase 'kds' and 'kdl'
     - an anisotropy factor of the Heyney-Greenstein sacattering function for both solad and liquid phase 'gs' and 'gl'
     - a refraction index for both solid and liquid phase 'ns' and 'nl'
-    - a ratio liquid over solid 'ls_r' """
+    - a ratio liquid over solid 'ls_r' 
+    - two phase solid ('phase' = 0) and liquid ('phase' = 1) """
 
-    def __init__(self, width, length, kas, kal, kds, kdl, gs, gl, ns, nl, ls_r):
+    def __init__(self, width, length, kas, kal, kds, kdl, gs, gl, ns, nl, ls_r, phase):
         """ inititialisation function for pcm """
         self.width = width
         self.length = length
         self.kas = kas
-        self.kad = kal
+        self.kal = kal
         self.kds = kds
         self.kdl = kdl
         self.gs = gs
@@ -31,26 +32,111 @@ class PcmStorage:
         self.ns = ns
         self.nl = nl
         self.ls_r = ls_r
+        self.phase = phase
 
+        
+        
         self.vect = np.array([1., 0.], float)
+        self.point_orig = np.array([0, width / 2.],float)
+        self.point_end = np.zeros(2)
+
+        self.front = self.ls_r * self.length
+        self.points = np.array([0, width / 2.],float)
         
-        self.points = np.empty([0, width / 2.],float)
+        self.khat = self.setKhat()
         
-        self.khat = self.setKhat(kas, kds, kal, kdl)
-
         
 
 
 
-    def setKhat(self, kas, kds, kal, kdl):
+    def setKhat(self):
         """ set the null collision coef """
+        return np.maximum(self.kas + self.kal, self.kal + self.kdl)
+
+
+
+
+    def getPathLength(self):
+        """ get a random path length """
+        return - np.log(np.random.rand()) / self.khat
+
+
+    def setRayEnd(self):
+        self.point_end = self.point_orig + self.vect * self.pathLength
+        print(self.pathLength)
+        print (self.point_end)
+    
+
+    def intersectWithBoundary(self):
+        """ compute intersection between a ray and the geometry """ 
+        out_x = out_y = True
+        length_x = length_y = self.length**2 + self.width**2
+        if self.point_end[0] < 0:
+            length_x = - self.point_orig[0] / self.vect[0]
+            self.point_end[0] = 0
+        elif self.point_end[0] > self.length :
+            length_x = (self.length - self.point_orig[0]) / self.vect[0]
+            self.point_end[0] = self.length
+        else:
+            out_x = False
+
+        if self.point_end[1] < 0:
+            length_y = - self.point_orig[1] / self.vect[1] 
+            self.point_end[1] = 0
+        elif self.point_end[1] > self.width :
+            length_y = (self.width - self.point_orig[1]) / self.vect[1]
+            self.point_end[1] = self.width
+        else:
+            out_y = False
+
+        if not out_x and not out_y :
+            return False
+        elif length_x < length_y:
+            self.pathLength = (self.point_end[0] - self.point_orig[0]) / self.vect[0]
+        else:
+            self.pathLength = (self.point_end[1] - self.point_orig[1]) / self.vect[1]
+
+        self.setRayEnd()
+        return True
+
+    def getPhaseChange(self):
+        length_x = self.length**2 + self.width**2
+        changePhase = False
         
-        return np.maximum(kas + kal,kal + kdl)
+        if self.phase == 0 and self.point_end[0] > self.front:
+            self.phase = 1
+            length_x = (self.front - self.point_orig[0]) / self.vect[0]
+            changePhase = True
+        if self.phase == 1 and self.point_end[0] < self.front:
+            self.phase = 0
+            length_x = (self.front - self.point_orig[0]) / self.vect[0]
+            changePhase = True
+            
+        if changePhase:
+            self.pathLength = (self.front - self.point_orig[0]) / self.vect[0]
+            self.setRayEnd()
 
+    def getInteraction(self):
+        pass
+        
 
+    def doPropagation(self):
+        out_of_pcm = False
+        
+        while not out_of_pcm:
+            self.pathLength = self.getPathLength()
+            self.setRayEnd()
+            out_of_pcm = self.intersectWithBoundary()
+            if out_of_pcm:
+                break
+            self.getPhaseChange()
+            print("phase  :", self.phase)
+            self.point_orig = self.point_end
 
-
-
+        print("sortie")
+        
+        
+        return 0.
     
 
 
@@ -58,6 +144,8 @@ class PcmStorage:
 
 
 
+class Ray:
+    """ 2D ray propagating inside the PCM """
 
-
-    
+    def __init__(self):
+        pass
